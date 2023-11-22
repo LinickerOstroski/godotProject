91e10598156c4bf132ba69dd0b43837750f22d3f
LínickerOstroski <olinicker@gmail.com>
2023-11-22 20:20:28 -0300

    espalhando as moedas

--- Mundo.gd
+++ Mundo.gd
@@ -11,15 +11,35 @@ func _ready():
 	print ("executou")
 	var Moeda1 = novaMoeda.instance()
 	var Moeda2 = novaMoeda.instance()
-	
+	var Moeda3 = novaMoeda.instance()
+	var Moeda4 = novaMoeda.instance()
+	var Moeda5 = novaMoeda.instance()
+	var Moeda6 = novaMoeda.instance()
 	
 	add_child(Moeda1)
 	add_child(Moeda2)
+	add_child(Moeda3)
+	add_child(Moeda4)
+	add_child(Moeda5)
+	add_child(Moeda6)
+	
 	Moeda1.position.x = 366
 	Moeda1.position.y = 751
 	
 	Moeda2.position.x = 18
 	Moeda2.position.y = 623
+	
+	Moeda3.position.x = 15
+	Moeda3.position.y = 184
+	
+	Moeda4.position.x = 287
+	Moeda4.position.y = -80
+	
+	Moeda5.position.x = 844
+	Moeda5.position.y = 354
+	
+	Moeda6.position.x = 917
+	Moeda6.position.y = 750
 	
 	pass # Replace with function body.
 
